c4401605f16d04ea67a2222859b1e9b8df15d1dc
Imre Bartis <imre.bartis@siili.com>
2018-02-02 13:08:57 +0200

    creating a post action to insert a customer

--- src/lib/customersRepository.js
+++ src/lib/customersRepository.js
@@ -25,19 +25,45 @@ class CustomersRepository {
         });
     }
 
+    // get a  customer
+    getCustomer(id, callback) {
+        console.log('*** CustomersRepository.getCustomer');
+        Customer.findById(id, (err, customer) => {
+            if (err) { 
+                console.log(`*** CustomersRepository.getCustomer error: ${err}`); 
+                return callback(err); 
+            }
+            callback(null, customer);
+        });
+    }
 
-        // get a  customer
-        getCustomer(id, callback) {
-            console.log('*** CustomersRepository.getCustomer');
-            Customer.findById(id, (err, customer) => {
-                if (err) { 
-                    console.log(`*** CustomersRepository.getCustomer error: ${err}`); 
-                    return callback(err); 
-                }
-                callback(null, customer);
-            });
-        }
+    // insert a  customer
+    insertCustomer(body, state, callback) {
+        console.log('*** CustomersRepository.insertCustomer');
+        console.log(state);
+        let customer = new Customer();
+        let newState = { 'id': state[0].id, 'abbreviation': state[0].abbreviation, 'name': state[0].name }
+        console.log(body);
 
+        customer.firstName = body.firstName;
+        customer.lastName = body.lastName;
+        customer.email = body.email;
+        customer.address = body.address;
+        customer.city = body.city;
+        customer.state = newState;
+        customer.stateId = newState.id;
+        customer.zip = body.zip;
+        customer.gender = body.gender;
+
+        customer.save((err, customer) => {
+            if (err) { 
+                console.log(`*** CustomersRepository insertCustomer error: ${err}`); 
+                return callback(err, null); 
+            }
+
+            callback(null, customer);
+        });
+    }
 }
 
 module.exports = new CustomersRepository();
